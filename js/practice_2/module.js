062185c8a156cd40f7a145010d3d1a10201db721
DARSHAN S KAMBLE <darshankamble7371@gmail.com>
2021-08-27 20:33:26 +0530

    practice set 2

---
+++ js/practice_2/module.js
@@ -0,0 +1,13 @@
+const add = (a, b) => { return a + b };
+const sub = (a, b) => { return a - b };
+const name = "Darshan"
+
+
+// EXPORT METHOD'S
+
+// module.exports = add;
+
+// module.exports.add = add;
+// module.exports.sub = sub;
+
+module.exports = { add, sub, name }
---
+++ js/practice_2/practice_2.js
@@ -0,0 +1,12 @@
+// IMPORT METHOD'S
+
+// const add = require("./module");
+
+// const { add, sub } = require("./module")
+
+const { sub, add, name } = require("./module");
+
+
+console.log(add(4, 5));
+console.log(sub(4, 5));
+console.log(name)
